db248a737df8abe571f0790f9d58c7a5a6565896
dsantosr <danielsantrodrigues42@gmail.com>
2025-05-12 22:05:54 -0300

    feat: tradutor sintático inicial

---
+++ src/main/java/br/ufma/Main.java
@@ -0,0 +1,9 @@
+package br.ufma;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        String input = "8+5-7+9";
+        Parser p = new Parser(input.getBytes());
+        p.parse();
+    }
+} 
